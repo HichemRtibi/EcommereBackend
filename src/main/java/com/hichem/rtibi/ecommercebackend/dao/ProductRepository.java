5dddd100d8461bf41093340bc0383a369b3eaaa8
hichem rtibi <hichem.rtibi@gmail.com>
2019-08-13 01:09:02 +0200

    chercher avec hpql equql findby

--- src/main/java/com/hichem/rtibi/ecommercebackend/dao/ProductRepository.java
+++ src/main/java/com/hichem/rtibi/ecommercebackend/dao/ProductRepository.java
@@ -2,13 +2,19 @@ package com.hichem.rtibi.ecommercebackend.dao;
 
 import com.hichem.rtibi.ecommercebackend.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
 @RepositoryRestResource
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     @RestResource(path = "/selectedProducts")
     public List<Product> findBySelectedIsTrue();
+
+    @RestResource(path = "/productsbykeyword")
+    public List<Product> findByNameContains(String mc);
+   /* @Query("select p from Product where p.name like: x")
+    public List<Product> chercher(String mc);*/
 }
