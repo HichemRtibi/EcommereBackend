165cf017602bbcbdd7e99bc810551a30b19fff6c
hichem rtibi <hichem.rtibi@gmail.com>
2019-08-12 20:40:43 +0200

    constructor

--- src/main/java/com/hichem/rtibi/ecommercebackend/entities/Category.java
+++ src/main/java/com/hichem/rtibi/ecommercebackend/entities/Category.java
@@ -10,7 +10,7 @@ import java.io.Serializable;
 import java.util.Collection;
 
 @Entity
-@Data @AllArgsConstructor @NoArgsConstructor @ToString
+
 public class Category implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,5 +20,48 @@ public class Category implements Serializable {
     @OneToMany(mappedBy = "category")
     private Collection<Product> products;
 
+    public Category(String name, String description, Collection<Product> products) {
+        this.name = name;
+        this.description = description;
+        this.products = products;
+    }
+    public Category(Long id,String name, String description, Collection<Product> products) {
+        this.id=id;
+        this.name = name;
+        this.description = description;
+        this.products = products;
+    }
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Collection<Product> getProducts() {
+        return products;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setProducts(Collection<Product> products) {
+        this.products = products;
+    }
 }
--- src/main/java/com/hichem/rtibi/ecommercebackend/entities/Product.java
+++ src/main/java/com/hichem/rtibi/ecommercebackend/entities/Product.java
@@ -1,8 +1,14 @@
 package com.hichem.rtibi.ecommercebackend.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.*;
 import java.io.Serializable;
 @Entity
+
 public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +22,91 @@ public class Product implements Serializable {
     private String photoName;
     @ManyToOne()
     private Category category;
+
+    public Product() {
+
+    }
+
+    public Product(String name, String description, double currentPrice, boolean promotion, boolean selected, double available, String photoName, Category category) {
+        this.name = name;
+        this.description = description;
+        this.currentPrice = currentPrice;
+        this.promotion = promotion;
+        this.selected = selected;
+        this.available = available;
+        this.photoName = photoName;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public boolean isPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(boolean promotion) {
+        this.promotion = promotion;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public double getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(double available) {
+        this.available = available;
+    }
+
+    public String getPhotoName() {
+        return photoName;
+    }
+
+    public void setPhotoName(String photoName) {
+        this.photoName = photoName;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
